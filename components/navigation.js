3cc3f2c6e2d9d5dd52b1ff96588c346a6c394737
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2022-03-05 16:19:27 +0000

    Navigation bar added, no icons included yet

---
+++ components/navigation.js
@@ -0,0 +1,37 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import FriendsScreen from '../screens/FriendsScreen';
+import DraftsScreen from '../screens/DraftsScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import styles from "../styling/Styles";
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+    return(
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 35,
+                    left: 20,
+                    right: 20,
+                    backgroundColor: '#F0F0F0',
+                    borderRadius: 15,
+                    height: 65,
+                    ...styles.shadow
+                },
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Search" component={SearchScreen}/>
+            <Tab.Screen name="Friends" component={FriendsScreen}/>
+            <Tab.Screen name="Drafts" component={DraftsScreen}/>
+            <Tab.Screen name="Settings" component={SettingsScreen}/>
+        </Tab.Navigator>
+    );
+}
