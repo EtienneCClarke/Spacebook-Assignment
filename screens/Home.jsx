85683ed766099ae1eeff026f8c16ed4d91bb906c
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2022-03-11 17:35:14 +0000

    Bug fixes

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import styles from '../styling/Styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { Component } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import UserLabel from '../components/UserLabel';
+import Posts from '../components/Posts';
+import Styles from '../styling/Styles';
+export default class Home extends Component {
 
-function Home() {
-    return (
-        <View style={styles.container}>
-            <Text>Home Screen</Text>
-            <Button
-                title="click me!"
-                onPress={() => alert('Button Clicked!')}
-            />
-        </View>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: null,
+            loading: true,
+        };
+    }
+
+    componentDidMount() {
+        AsyncStorage.getItem('@session_id').then((result) => {
+            this.setState({ id: result, loading: false });
+        });
+    }
+
+    render() {
+        if(this.state.loading) {
+            return (<View styles={[Styles.container, Styles.center]}><Text>Loading...</Text></View>);
+        } else {
+            return (
+                <View style={Styles.container}>
+                    <View style={Styles.header}>
+                        <UserLabel
+                            userId={this.state.id}
+                        />
+                    </View>
+                    <Text style={[Styles.title, {marginLeft: '5%', marginRight: '5%', paddingBottom: 15}]}>My Wall</Text>
+                    <ScrollView style={[Styles.container, {paddingTop: 0, marginBottom: 100}]}>
+                        <Posts targetID={this.state.id}/>
+                    </ScrollView>
+                </View>
+            );
+        }
+    }
 }
-
-export default Home;
