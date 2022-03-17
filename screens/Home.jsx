2c796b255eae5955ecfd86156d09a92819f70303
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2022-03-17 18:14:11 +0000

    Bug fix

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -43,7 +43,7 @@ export default class Home extends Component {
             return (
                 <View style={Styles.container}>
                     {this.state.showPostCard &&
-                        <PostCard closePostCard={this.closePostCard} target_wall={this.state.id}/>
+                        <PostCard closePostCard={this.closePostCard} target_wall={this.state.id} content={null}/>
                     }
                     <View style={Styles.header}>
                         <UserLabel
